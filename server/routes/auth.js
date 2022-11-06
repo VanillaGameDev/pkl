58c283748772117c5e8ddd35d831a4ab1f1b94d8
agamSuteguh <100204292+agamSuteguh@users.noreply.github.com>
2022-11-06 15:28:17 +0700

    UP

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,57 +12,35 @@ const authService = new AuthService()
   
 //register
 router.post("/register", authMiddleware.register, async (req, res) => {
+  try {
+    const result = await authService.register(req.body)
+    res.json(result)
+  } catch (error) {
+    console.log(error.message)
+    
+  }
  
 });
 
 //login
 router.post("/login", authMiddleware.login, async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    
-    const user = await User.findOne({ username });
+ try {
+  const result = await authService.login(req.body)
+  res.json(result)
+ } catch (error) {
+console.log(error.message)
+ 
 
-    const comparedPass = await bcrypt.compare(password, existUser.password);
-    if (!comparedPass) {
-      return res.json({
-        status: "bad",
-        msg: "password salah",
-      });
-    }
-
-    const token = await jwt.sign({ user }, process.env.TOKEN_KEYWORD);
-
-    res.json({
-      status: "ok",
-      msg: "berhasil login !",
-      user,
-      token,
-    });
-  } catch (error) {
-    console.log(error.message);
-  }
+ }
 });
 
 // admin login
 router.post('/admin', async (req, res) => {
-
-  try {
-    const { username, password } = req.body
-    
-    if(!username || !password){
- return res.json({status: 'bad', msg:'username atau password kosong!'})}
-
- if(!username !== process.env.ADMIN_LOGIN ){
-  return res.json({status: 'bad', msg:'username salah!'})}
- 
-  if(password !== process.env.ADMIN_PASS){
-    return res.json({status: 'bad', msg:'password salah!'})}
-
-    const token = jwt.sign({user: {username, password}}, process.env.TOKEN_KEYWORD)
-
-res.json({status: 'ok', msg: 'admin berhasil login', token})
-  } catch (error) {
-console.log(error.message)
-  }
+try {
+   const resuilt = await authService.admin(req.body)
+   res.json(result)
+} catch (error) {
+  console.log(error.message)
+}
 })
 module.exports = router;
--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -28,6 +28,52 @@ class authService{
             console.log(error.message);
           }
 }
+ async login(data) {
+  try {
+    const { username, password } = data;
+    
+    const user = await User.findOne({ username });
+
+    const comparedPass = await bcrypt.compare(password, existUser.password);
+    if (!comparedPass) {
+      return res.json({
+        status: "bad",
+        msg: "password salah",
+      });
+    }
+
+    const token = await jwt.sign({ user }, process.env.TOKEN_KEYWORD);
+
+    res.json({
+      status: "ok",
+      msg: "berhasil login !",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+ }
+ async admin(data){
+  try {
+    const { username, password } = data
+    
+    if(!username || !password){
+ return res.json({status: 'bad', msg:'username atau password kosong!'})}
+
+ if(!username !== process.env.ADMIN_LOGIN ){
+  return res.json({status: 'bad', msg:'username salah!'})}
+ 
+  if(password !== process.env.ADMIN_PASS){
+    return res.json({status: 'bad', msg:'password salah!'})}
+
+    const token = jwt.sign({user: {username, password}}, process.env.TOKEN_KEYWORD)
+
+res.json({status: 'ok', msg: 'admin berhasil login', token})
+  } catch (error) {
+console.log(error.message)
+  }
+ }
 }
 
 module.exports = authService
